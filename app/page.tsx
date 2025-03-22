5d5ed8af128f0ecd3c862f59e5ab607abf27a00f
gPhantasm <115580700+gPhantasm@users.noreply.github.com>
2025-03-22 19:33:38 -0400

    Large text class on certain buttons

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
               <p className="text-lg max-w-md">SmartCart revolutionizes grocery shopping with AI-powered, accessible, and seamless checkout solutions — making every trip faster, easier, and smarter for all.</p>
               <Button className="bg-[#5c5a7c] hover:bg-[#4a4865] text-white px-8 py-6 text-lg">Download</Button>
               <Link href="/checkout" className="inline-block mt-4">
-                <Button variant="outline" className="border-[#5c5a7c] py-6 text-[#5c5a7c]">
+                <Button variant="outline" className="text-lg border-[#5c5a7c] py-6 text-[#5c5a7c]">
                   Go to Checkout
                 </Button>
               </Link>
