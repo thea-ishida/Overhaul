5e02fce4f33686ab659e13cf836e4cfb567c9120
gPhantasm <115580700+gPhantasm@users.noreply.github.com>
2025-03-22 22:39:08 -0400

    About page: Julian

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,12 +5,10 @@ import { Bot } from "lucide-react";
 import Link from "next/link";
 import AccessibilityButton from "@/components/ui/accessibility-button";
 
-
-
 export default function About() {
   return (
     <div className="justify-center content-center">
-    <section className="bg-white text-black dark:bg-[#121212] dark:text-white transition-colors duration-300">
+      <section className="bg-white text-black dark:bg-[#121212] dark:text-white transition-colors duration-300">
         <div>
           <div className="container mx-auto px-4 py-16 md:py-24">
             <div className="grid md:grid-cols-2 gap-8 items-center">
@@ -66,7 +64,7 @@ export default function About() {
 
                     <ul className="list-disc">
                       <li>
-                        AI image detection using  <a className="text-orange-600">TensorFlow.js</a>
+                        AI image detection using <a className="text-orange-600">TensorFlow.js</a>
                       </li>
                       <li>Training a small machine learning model for image classification</li>
                       <li>Handling the styling and functionality of the checkout page</li>
@@ -76,6 +74,30 @@ export default function About() {
                 <div className="justify-center items-center flex flex-col">
                   <Image src="/headshot.jpg" alt="Evan" width={300} height={400} className="object-scale-down overflow-hidden rounded-lg drop-shadow-xl" />
                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col justify-center items-center">
+          <div className="grid grid-rows-1 grid-cols-1 container items-center mx-auto px-4 py-16 bg-[#c8c2f0] rounded-lg">
+            <div className="grid grid-rows1 grid-cols-2">
+              <div className="flex flex-col justify-center items-center">
+                <h1 className="justify-center text-3xl font-bold tracking-tight text-white">Julian Barua</h1>
+                <p className="items-center justify-center flex felx-col">My name is Julian. I am a second year computer science student at UWO. I enjoy making music, video games, and low-level programming.</p>
+
+                <div className="my-5 items-center flex flex-col justify-center">
+                  <p className="my-2">My primary responsiblities have been:</p>
+
+                  <ul className="list-disc">
+                    <li>Git repository management (Merge conflicts and branch control)</li>
+                    <li>Accessibility features (Text sizing)</li>
+                    <li>Testing and code review of authentication and checkout pages</li>
+                  </ul>
+                </div>
+              </div>
+              <div className="justify-center items-center flex flex-col">
+                <Image src="/julian.jpg" alt="Julian" width={300} height={300} className="object-scale-down overflow-hidden rounded-lg drop-shadow-xl" />
               </div>
             </div>
           </div>
