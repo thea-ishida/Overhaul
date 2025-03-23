5ccea5f41604d84426b0196ad9ea6170c14ca0e0
thea-ishida <thea.ishida@gmail.com>
2025-03-22 23:27:07 -0400

    added magnifying glass

--- app/mission/page.tsx
+++ app/mission/page.tsx
@@ -10,8 +10,17 @@ export default function Mission() {
     // h-screen makes this background fill the entire screen
     <div className="h-screen bg-gradient-to-br from-[#c8c2f0] via-[#8a82c5] to-[#5c5a7c]">
       <div className="container mx-auto py-20">
-        <h1 className="text-white text-5xl font-bold">OUR MISSION</h1>
-        <p>mission statement here</p>
+        <h1 className="text-white text-5xl font-bold mb-8">OUR MISSION</h1>
+        <div className="flex flex-row items-start space-x-8">
+          <Image src="/pexels.jpg" alt="Grocery store, courtesy of Pexels" width={500} height={400} className="object-scale-down overflow-hidden rounded-lg drop-shadow-xl" />
+          <div className="text-xl flex flex-col space-y-4">
+            <p>
+              At <b>SmartCart</b>, we’re reimagining the checkout experience with AI that’s fast, accurate, and built for everyone.
+            </p>
+            <p>Inspired by Faith’s hometown grocery roots, we’re creating accessible, human-centered technology that shortens lines, reduces friction, and makes shopping feel personal again.</p>
+            <p>Because innovation should start with people — not just data.</p>
+          </div>
+        </div>
       </div>
       <AccessibilityButton />
     </div>
